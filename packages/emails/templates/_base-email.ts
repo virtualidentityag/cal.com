e9e5bb5dcf01519ce74581c859a472d48dc951ef
Carlos Soares <carlos.soares17@gmail.com>
2023-04-03 14:37:56 +0100

    feat: adding the ability to prevent the send of emails OB-3774

--- packages/emails/templates/_base-email.ts
+++ packages/emails/templates/_base-email.ts
@@ -9,6 +9,16 @@ declare let global: {
   E2E_EMAILS?: Record<string, unknown>[];
 };
 
+interface EmailSettings {
+  emailNotificationsEnabled: boolean;
+  settings: {
+    initialEnquiryNotificationEnabled: boolean;
+    newChatMessageNotificationEnabled: boolean;
+    reassignmentNotificationEnabled: boolean;
+    appointmentNotificationEnabled: boolean;
+  };
+}
+
 export default class BaseEmail {
   name = "";
 
@@ -34,20 +44,100 @@ export default class BaseEmail {
       console.log("Skipped Sending Email as NEXT_PUBLIC_IS_E2E==1");
       return new Promise((r) => r("Skipped sendEmail for E2E"));
     }
-    new Promise((resolve, reject) =>
-      nodemailer
-        .createTransport(this.getMailerOptions().transport)
-        .sendMail(this.getNodeMailerPayload(), (_err, info) => {
-          if (_err) {
-            const err = getErrorFromUnknown(_err);
-            this.printNodeMailerError(err);
-            reject(err);
-          } else {
-            resolve(info);
-          }
-        })
-    ).catch((e) => console.error("sendEmail", e));
+    this.isAbleToSend().then((allowedToSend) => {
+      if (!allowedToSend) {
+        return;
+      }
+
+      new Promise((resolve, reject) =>
+        nodemailer
+          .createTransport(this.getMailerOptions().transport)
+          .sendMail(this.getNodeMailerPayload(), (_err, info) => {
+            if (_err) {
+              const err = getErrorFromUnknown(_err);
+              this.printNodeMailerError(err);
+              reject(err);
+            } else {
+              resolve(info);
+            }
+          })
+      ).catch((e) => console.error("sendEmail", e));
+    });
     return new Promise((resolve) => resolve("send mail async"));
+  }
+
+  protected async isAbleToSend(): Promise<boolean> {
+    const csrf = this.generateCsrfToken();
+    const token = await this.loginUserInApp();
+    const email = (this.getNodeMailerPayload().to as string).replace(/.*\<|\>/g, "");
+
+    if (!token) {
+      return false;
+    }
+
+    return fetch(
+      `${process.env.NEXT_PUBLIC_WEBAPP_URL}/service/users/notifications?email=${encodeURIComponent(email)}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+          Cookie: `CSRF-TOKEN=${csrf};`,
+          "x-csrf-token": csrf,
+        },
+      }
+    )
+      .then((r) => r.json())
+      .then((data: EmailSettings) => {
+        console.log(data);
+        return data.emailNotificationsEnabled && data.settings.appointmentNotificationEnabled;
+      })
+      .catch((e) => {
+        console.error(`Failed Getting user notifications for user: ${email}`, e);
+        return false;
+      });
+  }
+
+  private async loginUserInApp() {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+    headers.append("cache-control", "no-cache");
+
+    const formData = [
+      `username=${encodeURIComponent(process.env.CALCOM_TECHNICAL_USER as string)}`,
+      `password=${encodeURIComponent(process.env.CALCOM_TECHNICAL_PASSWORD as string)}`,
+      "client_id=app",
+      "grant_type=password",
+    ].join("&");
+    return fetch(
+      `${process.env.NEXT_PUBLIC_WEBAPP_URL}/auth/realms/online-beratung/protocol/openid-connect/token`,
+      {
+        method: "POST",
+        headers,
+        body: formData,
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => res.access_token)
+      .catch((e) => {
+        console.error(`Failed login into keycloack with calcom technical user`, e);
+        return null;
+      });
+  }
+
+  /**
+   * Generates the Csrf Token when not present
+   * @returns
+   */
+  private generateCsrfToken() {
+    let token = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < 18; i++) {
+      token += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return token;
   }
 
   protected getMailerOptions() {
