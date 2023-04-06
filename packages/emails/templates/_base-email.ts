b54cae737f1f78fbb8065e09111bdc1f2a197799
Carlos Soares <carlos.soares17@gmail.com>
2023-04-06 08:11:39 +0100

    fix: remove https from the request

--- packages/emails/templates/_base-email.ts
+++ packages/emails/templates/_base-email.ts
@@ -76,9 +76,7 @@ export default class BaseEmail {
     }
 
     return fetch(
-      `https://${process.env.NEXT_PUBLIC_WEBAPP_URL}/service/users/notifications?email=${encodeURIComponent(
-        email
-      )}`,
+      `${process.env.NEXT_PUBLIC_WEBAPP_URL}/service/users/notifications?email=${encodeURIComponent(email)}`,
       {
         method: "GET",
         headers: {
@@ -112,7 +110,7 @@ export default class BaseEmail {
       "grant_type=password",
     ].join("&");
     return fetch(
-      `https://${process.env.NEXT_PUBLIC_WEBAPP_URL}/auth/realms/online-beratung/protocol/openid-connect/token`,
+      `${process.env.NEXT_PUBLIC_WEBAPP_URL}/auth/realms/online-beratung/protocol/openid-connect/token`,
       {
         method: "POST",
         headers,
