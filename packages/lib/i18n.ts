d81410590969fbdf97a20f8d66cd7756dfc02ab2
Carlos Soares <carlos.soares17@gmail.com>
2023-05-23 10:26:27 +0100

    Trying to enforce the german OB-5003

--- packages/lib/i18n.ts
+++ packages/lib/i18n.ts
@@ -1,17 +1,8 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import parser from "accept-language-parser";
 import type { IncomingMessage } from "http";
 
-import type { Maybe } from "@calcom/trpc/server";
-
-const { i18n } = require("@calcom/config/next-i18next.config");
-
 export function getLocaleFromHeaders(req: IncomingMessage): string {
-  let preferredLocale: string | null | undefined;
-  if (req.headers["accept-language"]) {
-    preferredLocale = parser.pick(i18n.locales, req.headers["accept-language"]) as Maybe<string>;
-  }
-  return preferredLocale ?? i18n.defaultLocale;
+  return "de"; // preferredLocale ?? i18n.defaultLocale;
 }
 
 export const getDirFromLang = (locale: string | undefined) =>
