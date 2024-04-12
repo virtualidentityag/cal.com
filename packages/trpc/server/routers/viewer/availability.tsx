f4d7181eaa53dc68d142cd352d6ec86090b21945
janrembold <janrembold@gmx.net>
2024-04-12 16:20:03 +0200

    remove logs

--- packages/trpc/server/routers/viewer/availability.tsx
+++ packages/trpc/server/routers/viewer/availability.tsx
@@ -117,8 +117,6 @@ export const availabilityRouter = router({
             .toDate(),
         };
 
-        console.log('dateOverrides', override, newValue, timeZone);
-
         const dayRangeIndex = acc.findIndex(
           // early return prevents override.date from ever being empty.
           // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
